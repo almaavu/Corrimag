9ebc2fd8ecbbeb9225dcd18cf02989da9e54d8a5
almaavu <almaavu@gmail.com>
2021-09-30 10:58:38 +0200

    minor

---
+++ push.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+#git rm -r --cached .
+
+
+desc=minor
+#read -p "Commit description: " desc
+
+
+#read -p "Commit description: " desc
+
+
+git add .
+git commit -m "$desc"
+git push -u origin master
+
+git status
+            
