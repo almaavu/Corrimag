d64b6c9290f08abf3f93b6c43753704533e481ef
almaavu <almaavu@gmail.com>
2021-11-04 17:00:31 +0100

    .

--- test/create_test_images.py
+++ test/create_test_images.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python3
 
-
+import platform
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
  
+LETTERS = "bhoilx^"
 
-text = "a"
+# select font
+if platform.system() == "Windows":
+    fnt = ImageFont.truetype('Arial.ttf', 15) 
+else:    
+    fnt = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 80)  
 
-# fnt = ImageFont.truetype('Arial.ttf', 15)  # windows
-fnt = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 80)  # Linux
 
-for text in "bhoilx^":
+
+for text in LETTERS:
+    
+    # black background
     img = Image.new('RGB', (57, 80), color = 'black')
     d = ImageDraw.Draw(img)
+    
+    # draw white text
     d.text((0,0), text, "white", fnt)
+    
+    # blur
     img2 = img.filter(ImageFilter.GaussianBlur(radius=6))
+    
+    # save
     img2.save(f'{text}.jpg')
